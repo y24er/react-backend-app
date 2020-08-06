a1342b0612ba3cc5c67328d69e7b59d2b456e220
userName <gongl3730@gmail.com>
2020-08-06 10:14:46 +0800

    [TodoList][Bruno] feature: init project and add entity set get

--- src/main/java/com/oocl/todo/model/Todo.java
+++ src/main/java/com/oocl/todo/model/Todo.java
@@ -2,6 +2,33 @@ package com.oocl.todo.model;
 
 public class Todo {
     private Integer id;
-    private String value;
-    private Boolean completed;
+    private String content;
+    private Boolean status;
+
+    public Todo() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
