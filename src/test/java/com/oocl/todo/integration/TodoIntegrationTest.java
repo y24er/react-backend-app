b7fcd8c4130d6e7a6381f25570ae9cc5e7118f65
y24er <1028704486@qq.com>
2020-08-07 22:23:03 +0800

    [spring-boot-todo-list][Jessie] refactor: rename TodoIntegration to TodoIntegrationTest

--- src/test/java/com/oocl/todo/integration/TodoIntegrationTest.java
+++ src/test/java/com/oocl/todo/integration/TodoIntegrationTest.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class TodoIntegration {
+public class TodoIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
     @Autowired
